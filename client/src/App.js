9d3d4995e8d02d5bfc6e7ba538af7422317eeb59
nhanaa <nhanpro7d@gmail.com>
2022-12-29 22:17:00 +0000

    Created pages for login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<Signup />}/>
         </Routes>
       </div>
     </Router>
---
+++ client/src/pages/Login.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import './Auth.css'
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const {login, error, isLoading} = useLogin();
+
+  const handleSubmit = () => {
+
+  }
+  return (
+    <form onSubmit={handleSubmit} className="login">
+      <h2>Login</h2>
+      <div className="username">
+        <label>Username:</label>
+        <input
+          type="username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          placeholder="Enter username"
+        />
+      </div>
+      <div className="password">
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder="Enter password"
+        />
+      </div>
+      {/* <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error}</div>} */}
+    </form>
+  );
+}
+
+export default Login;
